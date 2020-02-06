ac87b7e0117960e03f24e024a5253e3338beb796
lludion <ulysse.remond@outlook.fr>
2020-02-06 11:47:14 +0100

    Added different types of Region #7
    -Desertic, Continental, Oceanic, Icy, Tundra

--- arenbels/game/region.py
+++ arenbels/game/region.py
@@ -30,3 +30,98 @@ class Region:
         elif season == 3:
             self.seasonBonus = self.winterBonus
             self.seasonHappy = self.winterHappy
+
+class Desertic(Region):
+    """ Very hot summers. Good food production all year round."""
+
+    def __init__(self,name="DeserticName",game=None):
+        super().__init__(name,game)
+
+        #Bonus to agrarian wealth
+        self.seasonBonus = 0
+        self.winterBonus = 30
+        self.springBonus = 10
+        self.summerBonus = 30
+        self.autumnBonus = 30
+
+        self.seasonHappy = 0
+        self.winterHappy = 1
+        self.springHappy = 1
+        self.summerHappy = -3
+        self.autumnHappy = 0
+
+class Continental(Region):
+    """ Sweltering summers, freezing winters, but good food prodution overall"""
+
+    def __init__(self,name="ContinentalName",game=None):
+        super().__init__(name,game)
+
+        #Bonus to agrarian wealth
+        self.seasonBonus = 0
+        self.winterBonus = -50
+        self.springBonus = 10
+        self.summerBonus = 30
+        self.autumnBonus = 60
+
+        self.seasonHappy = 0
+        self.winterHappy = -2
+        self.springHappy = 2
+        self.summerHappy = -1
+        self.autumnHappy = 1
+
+class Oceanic(Region):
+    """ Not many seasonal changes. Mild winters & summers """
+
+    def __init__(self,name="OceanicName",game=None):
+        super().__init__(name,game)
+
+        #Bonus to agrarian wealth
+        self.seasonBonus = 0
+        self.winterBonus = -10
+        self.springBonus = 10
+        self.summerBonus = 20
+        self.autumnBonus = 30
+
+        self.seasonHappy = 0
+        self.winterHappy = -1
+        self.springHappy = 0
+        self.summerHappy = 1
+        self.autumnHappy = 0
+
+class Icy(Region):
+    """ Happiness bonuses, but heavy maluses to agriculture"""
+
+    def __init__(self,name="IcyName",game=None):
+        super().__init__(name,game)
+
+        #Bonus to agrarian wealth
+        self.seasonBonus = 0
+        self.winterBonus = -100
+        self.springBonus = -50
+        self.summerBonus = -20
+        self.autumnBonus = -30
+
+        self.seasonHappy = 0
+        self.winterHappy = 2
+        self.springHappy = 1
+        self.summerHappy = 1
+        self.autumnHappy = 1
+
+class Tundra(Region):
+    """ Rough winter, bad springs, happy summer& autumn"""
+
+    def __init__(self,name="IcyName",game=None):
+        super().__init__(name,game)
+
+        #Bonus to agrarian wealth
+        self.seasonBonus = 0
+        self.winterBonus = -30
+        self.springBonus = -20
+        self.summerBonus = 30
+        self.autumnBonus = 30
+
+        self.seasonHappy = 0
+        self.winterHappy = -2
+        self.springHappy = 0
+        self.summerHappy = 1
+        self.autumnHappy = 1
